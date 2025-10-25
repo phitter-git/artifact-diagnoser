2c795ea432a99028bdd8c1ac8e1b0d4831cb6e7d
phitter <phitter.git@gmail.com>
2025-10-26 00:08:27 +0900

    ビルド

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2373542897"
+    serviceWorkerVersion: "434365654"
   }
 });
